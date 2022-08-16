613c2136d9bf760771520dc3f7aa290c9dd5ac7d
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2022-08-16 22:01:15 +0530

    Add files via upload

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -4,7 +4,13 @@ const sidebar=document.querySelector('.sidebar')
 const closeBtn=document.querySelector('.close-btn')
 
 sidebarToggle.addEventListener('click',()=>{
-    console.log("yes")
+  // This is will toggle the show sidebar class
+//   if(sidebar.classList.contains('show-sidebar')){
+//     sidebar.classList.remove('show-sidebar')
+//   }
+//   else{
+//     sidebar.classList.add('show-sidebar')
+//   }
     sidebar.classList.toggle('show-sidebar')
 })
 closeBtn.addEventListener('click',()=>{
